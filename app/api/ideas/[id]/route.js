89035adb5ff88397f8d52e6871f82b3b49c4f575
Benjamin Alcade <alcade.benjamin@gmail.com>
2023-08-11 15:34:10 +0200

    app done

--- app/api/ideas/[id]/route.js
+++ app/api/ideas/[id]/route.js
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server"
+import ideas from "../ideas.json"
 
-export async function GET(request, context) {
+export async function GET(request, context) 
+ {
+    const { id } = context.params;
+    const idea = ideas.find((idea) => idea.id === id);
     return NextResponse.json({
-        msg: 'Hello from the API'
+        idea,
+        msg: `retrieved param from end point ${id}`,
     });
 
 } 
--- app/ideas/[id]/page.jsx
+++ app/ideas/[id]/page.jsx
@@ -1,3 +1,16 @@
 async function fetchIdea(id) {
-    
+    const URL = `http://localhost:3000/api/ideas/`;
+    const res = await fetch(`${URL}/${id}`);
+    const idea = res.json();
+    return idea;
+}
+
+export default async function Page({params}) {
+    const { id } = params ;
+    const idea =  await fetchIdea(id);
+    return (
+        <main className="main">
+            {JSON.stringify(idea, null, 2)}
+        </main>
+    )
 }
